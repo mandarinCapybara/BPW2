faf515915d08024cbc5cc5a5dd81ce439b2de1d6
mandarinCapybara <mitchelgeraedts@gmail.com>
2024-02-18 16:31:30 +0100

    Wires added

---
+++ BPW 2/Assets/Scripts/Support/Wire.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wire : MonoBehaviour
+{
+    [SerializeField] private List<MeshRenderer> renderers;
+    [SerializeField] private Material poweredMat;
+    [SerializeField] private Material unpoweredMat;
+
+    [SerializeField] private CoilElectrified parent;
+
+    private bool powered;
+    private bool poweredStored;
+
+    private void Update()
+    {
+        powered = parent.GetElectrified(); 
+        if(powered !=  poweredStored)
+        {
+            UpdateMaterials();
+        }
+    }
+
+    private void UpdateMaterials()
+    {
+        poweredStored = powered;
+        foreach (MeshRenderer r in renderers)
+        {
+            Material[] storedMats = r.materials;
+
+            if (powered == false)
+            storedMats[1] = unpoweredMat;
+
+            else if (powered)
+                storedMats[1] = poweredMat;
+
+            r.materials = storedMats;
+        }
+    }
+}
